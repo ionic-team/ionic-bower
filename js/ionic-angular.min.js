bddc09bcd57b72c5f5e89c643cefcf99993a0965
Ionotron <hi@ionicframework.com>
2014-02-05 17:41:39 +0000

    release: v0.9.23-663

--- js/ionic-angular.min.js
+++ js/ionic-angular.min.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.9.23-662
+ * Ionic, v0.9.23-663
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
--- js/ionic.js
+++ js/ionic.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.9.23-662
+ * Ionic, v0.9.23-663
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -16,7 +16,7 @@
 window.ionic = {
   controllers: {},
   views: {},
-  version: '0.9.23-662'
+  version: '0.9.23-663'
 };;
 (function(ionic) {
 
