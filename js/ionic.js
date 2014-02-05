1e345ef3bafc756d43b2132fa840929e6c601ed6
Ionotron <hi@ionicframework.com>
2014-02-05 16:58:29 +0000

    release: v0.9.23-658

--- js/ionic.js
+++ js/ionic.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.9.23-alpha-657
+ * Ionic, v0.9.23-658
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -16,7 +16,7 @@
 window.ionic = {
   controllers: {},
   views: {},
-  version: '0.9.23-alpha-657'
+  version: '0.9.23-658'
 };;
 (function(ionic) {
 
