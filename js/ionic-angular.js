197406ccac804da19fe27ae9298bffac2981e166
Ionotron <hi@ionicframework.com>
2014-02-10 13:57:09 +0000

    release: v0.9.24-alpha-725

--- js/ionic-angular.js
+++ js/ionic-angular.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.9.24-alpha-724
+ * Ionic, v0.9.24-alpha-725
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
