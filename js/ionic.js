048ed16c8178e80a3d14aa3a9f3c586bcbc248a6
Ionotron <hi@ionicframework.com>
2014-02-17 22:17:30 +0000

    release: v0.9.25-alpha-832

--- js/ionic.js
+++ js/ionic.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.9.25-alpha-831
+ * Ionic, v0.9.25-alpha-832
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -17,7 +17,7 @@
 window.ionic = {
   controllers: {},
   views: {},
-  version: '0.9.25-alpha-831'
+  version: '0.9.25-alpha-832'
 };;
 (function(ionic) {
 
