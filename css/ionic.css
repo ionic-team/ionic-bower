902a536af506dc85950568132022dbfb61fd2a47
Ionotron <hi@ionicframework.com>
2014-02-21 16:53:45 +0000

    release: v0.9.26-alpha-894

--- css/ionic.css
+++ css/ionic.css
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.9.26-alpha-892
+ * Ionic, v0.9.26-alpha-894
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
