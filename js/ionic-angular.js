40cf6c8b8595343ec74f6453f90400186cea914e
Ionotron <hi@ionicframework.com>
2014-03-03 21:35:06 +0000

    release: v0.10.0-alpha-1033

--- js/ionic-angular.js
+++ js/ionic-angular.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.10.0-alpha-1032
+ * Ionic, v0.10.0-alpha-1033
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -3022,12 +3022,14 @@ angular.module('ionic.ui.viewState', ['ionic.service.view', 'ionic.service.gestu
           $scope.oldTitle = $scope.title;
           $scope.title = data && data.title || '';
 
-          //If no animation, we're done!
-          if (!$scope.animateEnabled) {
-            hb.align();
-            return;
-          } else {
-            animateTitles();
+          // only change if they're different
+          if($scope.oldTitle !== $scope.title) {
+            if (!$scope.animateEnabled) {
+              //If no animation, we're done!
+              hb.align();
+            } else {
+              animateTitles();
+            }
           }
         }
 
@@ -3038,7 +3040,7 @@ angular.module('ionic.ui.viewState', ['ionic.service.view', 'ionic.service.gestu
           //replace the first one with an oldTitle element
           currentTitles = $element[0].querySelectorAll('.title');
           if (currentTitles.length) {
-            oldTitleEl = $compile('<h1 ng-bind-html="oldTitle" class="title"></h1>')($scope);
+            oldTitleEl = $compile('<h1 class="title" ng-bind-html="oldTitle"></h1>')($scope);
             angular.element(currentTitles[0]).replaceWith(oldTitleEl);
           }
           //Compile new title
