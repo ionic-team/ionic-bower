d6aa01886bab842caa40a694e2fdace3df3ee39f
Ionotron <hi@ionicframework.com>
2014-03-04 18:16:34 +0000

    release: v0.10.0-alpha-1040

--- css/ionic.min.css
+++ css/ionic.min.css
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.10.0-alpha-1038
+ * Ionic, v0.10.0-alpha-1040
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
--- js/ionic.js
+++ js/ionic.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.10.0-alpha-1038
+ * Ionic, v0.10.0-alpha-1040
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -18,7 +18,7 @@
 window.ionic = {
   controllers: {},
   views: {},
-  version: '0.10.0-alpha-1038'
+  version: '0.10.0-alpha-1040'
 };
 ;
 (function(ionic) {
