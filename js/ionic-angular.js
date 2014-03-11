322fa0136f6351b32e59a964847f742bf6220e9d
Ionotron <hi@ionicframework.com>
2014-03-11 20:04:04 +0000

    release: v0.10.0-alpha-nightly-1127

--- js/ionic-angular.js
+++ js/ionic-angular.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.10.0-alpha-nightly-1126
+ * Ionic, v0.10.0-alpha-nightly-1127
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -2218,8 +2218,6 @@ function($parse, $timeout, $ionicScrollDelegate, $controller, $ionicBind) {
       var scrollCtrl = ctrls[0];
       var infiniteScrollCtrl = ctrls[1];
       var scrollView = infiniteScrollCtrl.scrollView = scrollCtrl.scrollView;
-
-      var iconElement = $element[0].querySelector('.icon');
 
       $scope.icon = function() {
         return angular.isDefined($attrs.icon) ? $attrs.icon : 'ion-loading-d';
