b540f1e0c4cdd9c6aeb282ddce1c7f6cd2c4ee03
Ionotron <hi@ionicframework.com>
2014-03-13 20:07:13 +0000

    release: v0.10.0-alpha-nightly-1180

--- js/ionic.js
+++ js/ionic.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.10.0-alpha-nightly-1177
+ * Ionic, v0.10.0-alpha-nightly-1180
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -5661,45 +5661,6 @@ ionic.views.Scroll = ionic.views.View.inherit({
         // Remove the back button if it's there
         this._currentBackButton.parentNode.removeChild(this._currentBackButton);
       }
-    }
-  });
-
-})(ionic);
-
-(function(ionic) {
-'use strict';
-  /**
-   * An ActionSheet is the slide up menu popularized on iOS.
-   *
-   * You see it all over iOS apps, where it offers a set of options 
-   * triggered after an action.
-   */
-  ionic.views.Popup = ionic.views.View.inherit({
-    initialize: function(opts) {
-      var _this = this;
-
-      this.el = opts.el;
-    },
-
-    setTitle: function(title) {
-      var titleEl = el.querySelector('.popup-title');
-      if(titleEl) {
-        titleEl.innerHTML = title;
-      }
-    },
-    alert: function(message) {
-      var _this = this;
-
-      ionic.requestAnimationFrame(function() {
-        _this.setTitle(message);
-        _this.el.classList.add('active');
-      });
-    },
-    hide: function() {
-      // Force a reflow so the animation will actually run
-      this.el.offsetWidth;
-
-      this.el.classList.remove('active');
     }
   });
 
