75a83a6ff48711aca691795a26b59505397eb725
Ionotron <hi@ionicframework.com>
2014-03-14 04:20:01 +0000

    release: v0.10.0-alpha-nightly-1195

--- js/ionic.js
+++ js/ionic.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.10.0-alpha-nightly-1194
+ * Ionic, v0.10.0-alpha-nightly-1195
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
