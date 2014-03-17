b646ed86aee287e12f9abb6f86019341c56766a4
Ionotron <hi@ionicframework.com>
2014-03-17 02:26:54 +0000

    release: v0.9.27-nightly-1239

--- js/ionic-angular.min.js
+++ js/ionic-angular.min.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.9.27-nightly-1237
+ * Ionic, v0.9.27-nightly-1239
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
--- js/ionic.js
+++ js/ionic.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.9.27-nightly-1237
+ * Ionic, v0.9.27-nightly-1239
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -3558,9 +3558,6 @@ ionic.views.Scroll = ionic.views.View.inherit({
   __resizeScrollbars: function() {
     var self = this;
 
-    // Bring the scrollbars in to show the content change
-    self.__fadeScrollbars('in');
-
     // Update horiz bar
     if(self.__indicatorX) {
       var width = Math.max(Math.round(self.__clientWidth * self.__clientWidth / (self.__contentWidth)), 20);
@@ -4175,8 +4172,6 @@ ionic.views.Scroll = ionic.views.View.inherit({
 
     var self = this;
 
-    self.__fadeScrollbars('in');
-
     // Reset interruptedAnimation flag
     self.__interruptedAnimation = true;
 
@@ -4419,6 +4414,7 @@ ionic.views.Scroll = ionic.views.View.inherit({
       self.__isDragging = (self.__enableScrollX || self.__enableScrollY) && (distanceX >= minimumTrackingForDrag || distanceY >= minimumTrackingForDrag);
       if (self.__isDragging) {
         self.__interruptedAnimation = false;
+        self.__fadeScrollbars('in');
       }
 
     }
