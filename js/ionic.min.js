0f085567c4e656373b4fc0f302317ed79a6ee1d6
Ionotron <hi@ionicframework.com>
2014-03-19 20:23:44 +0000

    release: v0.9.27-nightly-1310

--- js/ionic.min.js
+++ js/ionic.min.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.9.27-nightly-1309
+ * Ionic, v0.9.27-nightly-1310
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
