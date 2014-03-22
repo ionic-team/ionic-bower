3f20a047017a69f221c124c2d89b533e4639e349
Ionotron <hi@ionicframework.com>
2014-03-22 14:59:38 +0000

    release: v0.9.27-nightly-1344

--- js/ionic-angular.js
+++ js/ionic-angular.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.9.27-nightly-1343
+ * Ionic, v0.9.27-nightly-1344
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -1942,28 +1942,28 @@ angular.module('ionic.decorator.location', [])
  * @private
  */
 .config(['$provide', function($provide) {
+  function $LocationDecorator($location, $timeout) {
+
+    $location.__hash = $location.hash;
+    //Fix: when window.location.hash is set, the scrollable area
+    //found nearest to body's scrollTop is set to scroll to an element
+    //with that ID.
+    $location.hash = function(value) {
+      if (angular.isDefined(value)) {
+        $timeout(function() {
+          var scroll = document.querySelector('.scroll-content');
+          if (scroll)
+            scroll.scrollTop = 0;
+        }, 0, false);
+      }
+      return $location.__hash(value);
+    };
+
+    return $location;
+  }
+  
   $provide.decorator('$location', ['$delegate', '$timeout', $LocationDecorator]);
 }]);
-
-function $LocationDecorator($location, $timeout) {
-
-  $location.__hash = $location.hash;
-  //Fix: when window.location.hash is set, the scrollable area
-  //found nearest to body's scrollTop is set to scroll to an element
-  //with that ID.
-  $location.hash = function(value) {
-    if (angular.isDefined(value)) {
-      $timeout(function() {
-        var scroll = document.querySelector('.scroll-content');
-        if (scroll)
-          scroll.scrollTop = 0;
-      }, 0, false);
-    }
-    return $location.__hash(value);
-  };
-
-  return $location;
-}
 
 (function() {
 'use strict';
