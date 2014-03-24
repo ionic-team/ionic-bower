c882b1e7d16d66667bd471ab87d249c261dd7eaf
Ionotron <hi@ionicframework.com>
2014-03-24 13:50:18 +0000

    release: v0.9.27-nightly-1347

--- js/ionic-angular.js
+++ js/ionic-angular.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.9.27-nightly-1346
+ * Ionic, v0.9.27-nightly-1347
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
--- js/ionic.js
+++ js/ionic.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v0.9.27-nightly-1346
+ * Ionic, v0.9.27-nightly-1347
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -18,7 +18,7 @@
 window.ionic = {
   controllers: {},
   views: {},
-  version: '0.9.27-nightly-1346'
+  version: '0.9.27-nightly-1347'
 };
 
 (function(ionic) {
