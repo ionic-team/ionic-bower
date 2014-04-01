9734d534f8436c6e79debd0a5d4025351f035569
Ionotron <hi@ionicframework.com>
2014-04-01 05:01:27 +0000

    release: v1.0.0-beta.1-nightly-1508

--- js/ionic-angular.js
+++ js/ionic-angular.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.1-nightly-1507
+ * Ionic, v1.0.0-beta.1-nightly-1508
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -905,6 +905,7 @@ angular.module('ionic.service.platform', [])
          * Trigger a callback once the device is ready,
          * or immediately if the device is already ready.
          * @param {function=} callback The function to call.
+         * @returns {promise} A promise which is resolved when the device is ready.
          */
         ready: function(cb) {
           var q = $q.defer();
--- js/ionic.bundle.js
+++ js/ionic.bundle.js
@@ -8,7 +8,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.1-nightly-1507
+ * Ionic, v1.0.0-beta.1-nightly-1508
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -25,7 +25,7 @@
 window.ionic = {
   controllers: {},
   views: {},
-  version: '1.0.0-beta.1-nightly-1507'
+  version: '1.0.0-beta.1-nightly-1508'
 };
 
 (function(ionic) {
@@ -32183,7 +32183,7 @@ angular.module('ui.router.compat')
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.1-nightly-1507
+ * Ionic, v1.0.0-beta.1-nightly-1508
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -33086,6 +33086,7 @@ angular.module('ionic.service.platform', [])
          * Trigger a callback once the device is ready,
          * or immediately if the device is already ready.
          * @param {function=} callback The function to call.
+         * @returns {promise} A promise which is resolved when the device is ready.
          */
         ready: function(cb) {
           var q = $q.defer();
