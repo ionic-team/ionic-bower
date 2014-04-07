0d884f7878191c94f9788ed3d410e46d82ef97aa
Ionotron <hi@ionicframework.com>
2014-04-07 14:49:16 +0000

    release: v1.0.0-beta.1-nightly-1581

--- js/ionic.js
+++ js/ionic.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.1-nightly-1580
+ * Ionic, v1.0.0-beta.1-nightly-1581
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -19,7 +19,7 @@
 window.ionic = {
   controllers: {},
   views: {},
-  version: '1.0.0-beta.1-nightly-1580'
+  version: '1.0.0-beta.1-nightly-1581'
 };
 
 (function(ionic) {
@@ -2365,6 +2365,7 @@ window.ionic = {
   var startCoordinates = {}; // used to remember where the coordinates of the start of a touch
   var clickPreventTimerId;
   var _hasTouchScrolled = false; // if the touchmove already exceeded the touchmove tolerance
+  var _activeElement; // the element which has focus
 
 
   ionic.tap = {
@@ -2428,15 +2429,8 @@ window.ionic = {
 
       ele.dispatchEvent(clickEvent);
 
-      if( ele.tagName.match(/input|textarea/i) ) {
-        ele.focus();
-        e.preventDefault();
-      } else if( ele.tagName == 'SELECT' ) {
-        // select simulateClick should not preventDefault or else no options dialog
-        ele.focus();
-      } else {
-        ionic.tap.blurActive();
-      }
+      // if it's an input, focus in on the target, otherwise blur
+      ionic.tap.handleFocus(ele);
 
       // remember the coordinates of this tap so if it happens again we can ignore it
       // but only if the coordinates are not already being actively disabled
@@ -2452,7 +2446,20 @@ window.ionic = {
     },
 
     ignoreSimulateClick: function(ele) {
-      return ele.disabled || ele.type === 'file' || ele.type === 'range';
+      return ele.disabled || ele.type === 'range';
+    },
+
+    handleFocus: function(ele) {
+      if(ionic.tap.activeElement() !== ele) {
+        // only set the focus if it doesn't already have it
+        if( ele.tagName.match(/input|textarea|select/i) ) {
+          ionic.tap.activeElement(ele);
+          ele.focus();
+        } else {
+          ionic.tap.activeElement(null);
+          ionic.tap.blurActive();
+        }
+      }
     },
 
     preventGhostClick: function(e) {
@@ -2552,14 +2559,20 @@ window.ionic = {
     },
 
     blurActive: function() {
-      var ele = document.activeElement;
-      if(ele && (ele.tagName === "INPUT" ||
-                 ele.tagName === "TEXTAREA")) {
-        // using a timeout to prevent funky scrolling while a keyboard hides
+      var ele = ionic.tap.activeElement();
+      if(ele && ele.tagName.match(/input|textarea|select/i) ) {
         setTimeout(function(){
           ele.blur();
+          ionic.tap.activeElement(null);
         }, 400);
       }
+    },
+
+    activeElement: function(ele) {
+      if(arguments.length) {
+        _activeElement = ele;
+      }
+      return _activeElement || document.activeElement;
     },
 
     setTouchStart: function(e) {
