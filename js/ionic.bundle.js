0dd2c1d74b8a2f9f89a81291ab70567de36b4b80
Ionotron <hi@ionicframework.com>
2014-04-19 21:50:46 +0000

    release: v1.0.0-beta.1-nightly-1744

--- js/ionic.bundle.js
+++ js/ionic.bundle.js
@@ -9,7 +9,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.1-nightly-1742
+ * Ionic, v1.0.0-beta.1-nightly-1744
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -26,7 +26,7 @@
 window.ionic = {
   controllers: {},
   views: {},
-  version: '1.0.0-beta.1-nightly-1742'
+  version: '1.0.0-beta.1-nightly-1744'
 };
 
 (function(ionic) {
@@ -3570,7 +3570,7 @@ ionic.views.Scroll = ionic.views.View.inherit({
       penetrationAcceleration : 0.08,
 
       // The ms interval for triggering scroll events
-      scrollEventInterval: 50
+      scrollEventInterval: 20
     };
 
     for (var key in options) {
@@ -31959,7 +31959,7 @@ angular.module('ui.router.compat')
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.1-nightly-1742
+ * Ionic, v1.0.0-beta.1-nightly-1744
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
--- js/ionic.js
+++ js/ionic.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.1-nightly-1742
+ * Ionic, v1.0.0-beta.1-nightly-1744
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -19,7 +19,7 @@
 window.ionic = {
   controllers: {},
   views: {},
-  version: '1.0.0-beta.1-nightly-1742'
+  version: '1.0.0-beta.1-nightly-1744'
 };
 
 (function(ionic) {
@@ -3563,7 +3563,7 @@ ionic.views.Scroll = ionic.views.View.inherit({
       penetrationAcceleration : 0.08,
 
       // The ms interval for triggering scroll events
-      scrollEventInterval: 50
+      scrollEventInterval: 20
     };
 
     for (var key in options) {
