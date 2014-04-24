58b08e5526be385345537431c6d5d7a8f93b557c
Ionotron <hi@ionicframework.com>
2014-04-24 15:36:06 +0000

    release: v1.0.0-beta.1-nightly-1826

--- css/ionic.css
+++ css/ionic.css
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.1-nightly-1825
+ * Ionic, v1.0.0-beta.1-nightly-1826
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
