edafb3983320ccf4041134cc8357fe71e42083c5
Ionitron <hi@ionicframework.com>
2014-05-07 18:30:28 +0000

    release: v1.0.0-beta.4-nightly-2010

--- js/ionic.bundle.js
+++ js/ionic.bundle.js
@@ -9,7 +9,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.4-nightly-2011
+ * Ionic, v1.0.0-beta.4-nightly-2010
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -26,7 +26,7 @@
 window.ionic = {
   controllers: {},
   views: {},
-  version: '1.0.0-beta.4-nightly-2011'
+  version: '1.0.0-beta.4-nightly-2010'
 };
 
 (function(ionic) {
@@ -37387,7 +37387,7 @@ angular.module('ui.router.compat')
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.4-nightly-2011
+ * Ionic, v1.0.0-beta.4-nightly-2010
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
