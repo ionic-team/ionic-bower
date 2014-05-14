90f797324966bad4f57d7c04d4a1ab00cf602389
Ionitron <hi@ionicframework.com>
2014-05-14 17:21:16 +0000

    release: v1.0.0-beta.5-nightly-2104

--- js/ionic.bundle.js
+++ js/ionic.bundle.js
@@ -9,7 +9,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.5-nightly-2103
+ * Ionic, v1.0.0-beta.5-nightly-2104
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -26,7 +26,7 @@
 window.ionic = {
   controllers: {},
   views: {},
-  version: '1.0.0-beta.5-nightly-2103'
+  version: '1.0.0-beta.5-nightly-2104'
 };
 
 (function(ionic) {
@@ -7819,7 +7819,7 @@ ionic.views.Slider = ionic.views.View.inherit({
       var tf;
 
       if(typeof opts.curve === 'string') {
-        tf = ionic.Animation.TimingFn[opts.curve] || ionic.Animation.TimingFn['linear'];
+        tf = ionic.Animation.TimingFn[opts.curve] || ionic.Animation.TimingFn.linear;
         if(opts.curve.indexOf('cubic-bezier(') >= 0) {
           var parts = opts.curve.replace('cubic-bezier(', '').replace(')', '').split(',');
           tf = ionic.Animation.TimingFn['cubic-bezier'];
@@ -7944,7 +7944,7 @@ ionic.views.Slider = ionic.views.View.inherit({
  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
  * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 (function(ionic) {
 
@@ -7959,23 +7959,23 @@ ionic.views.Slider = ionic.views.View.inherit({
   function B3(t) { return 3*t*(1-t)*(1-t); }
   function B4(t) { return (1-t)*(1-t)*(1-t); }
 
-  ionic.Animation = ionic.Animation || {}
+  ionic.Animation = ionic.Animation || {};
 
- 
+
   /**
    * JavaScript port of Webkit implementation of CSS cubic-bezier(p1x.p1y,p2x,p2y) by http://mck.me
    * http://svn.webkit.org/repository/webkit/trunk/Source/WebCore/platform/graphics/UnitBezier.h
    */
   ionic.Animation.Bezier = (function(){
     'use strict';
-   
+
     /**
      * Duration value to use when one is not specified (400ms is a common value).
      * @const
      * @type {number}
      */
     var DEFAULT_DURATION = 400;//ms
-   
+
     /**
      * The epsilon value we pass to UnitBezier::solve given that the animation is going to run over |dur| seconds.
      * The longer the animation, the more precision we need in the timing function result to avoid ugly discontinuities.
@@ -7984,7 +7984,7 @@ ionic.views.Slider = ionic.views.View.inherit({
     var solveEpsilon = function(duration) {
       return 1.0 / (200.0 * duration);
     };
-   
+
     /**
      * Defines a cubic-bezier curve given the middle two control points.
      * NOTE: first and last control points are implicitly (0,0) and (1,1).
@@ -7994,53 +7994,53 @@ ionic.views.Slider = ionic.views.View.inherit({
      * @param p2y {number} Y component of control point 2
      */
     var unitBezier = function(p1x, p1y, p2x, p2y) {
-    
+
       // private members --------------------------------------------
-   
+
       // Calculate the polynomial coefficients, implicit first and last control points are (0,0) and (1,1).
-   
+
       /**
        * X component of Bezier coefficient C
        * @const
        * @type {number}
        */
       var cx = 3.0 * p1x;
-   
+
       /**
        * X component of Bezier coefficient B
        * @const
        * @type {number}
        */
       var bx = 3.0 * (p2x - p1x) - cx;
-   
+
       /**
        * X component of Bezier coefficient A
        * @const
        * @type {number}
        */
       var ax = 1.0 - cx -bx;
-   
+
       /**
        * Y component of Bezier coefficient C
        * @const
        * @type {number}
        */
       var cy = 3.0 * p1y;
-   
+
       /**
        * Y component of Bezier coefficient B
        * @const
        * @type {number}
        */
       var by = 3.0 * (p2y - p1y) - cy;
-   
+
       /**
        * Y component of Bezier coefficient A
        * @const
        * @type {number}
        */
       var ay = 1.0 - cy - by;
-   
+
       /**
        * @param t {number} parametric timing value
        * @return {number}
@@ -8049,7 +8049,7 @@ ionic.views.Slider = ionic.views.View.inherit({
         // `ax t^3 + bx t^2 + cx t' expanded using Horner's rule.
         return ((ax * t + bx) * t + cx) * t;
       };
-   
+
       /**
        * @param t {number} parametric timing value
        * @return {number}
@@ -8057,7 +8057,7 @@ ionic.views.Slider = ionic.views.View.inherit({
       var sampleCurveY = function(t) {
         return ((ay * t + by) * t + cy) * t;
       };
-   
+
       /**
        * @param t {number} parametric timing value
        * @return {number}
@@ -8065,7 +8065,7 @@ ionic.views.Slider = ionic.views.View.inherit({
       var sampleCurveDerivativeX = function(t) {
         return (3.0 * ax * t + 2.0 * bx) * t + cx;
       };
-   
+
       /**
        * Given an x value, find a parametric value it came from.
        * @param x {number} value of x along the bezier curve, 0.0 <= x <= 1.0
@@ -8079,7 +8079,7 @@ ionic.views.Slider = ionic.views.View.inherit({
         var x2;
         var d2;
         var i;
-   
+
         // First try a few iterations of Newton's method -- normally very fast.
         for (t2 = x, i = 0; i < 8; i++) {
           x2 = sampleCurveX(t2) - x;
@@ -8092,19 +8092,19 @@ ionic.views.Slider = ionic.views.View.inherit({
           }
           t2 = t2 - x2 / d2;
         }
-   
+
         // Fall back to the bisection method for reliability.
         t0 = 0.0;
         t1 = 1.0;
         t2 = x;
-   
+
         if (t2 < t0) {
           return t0;
         }
         if (t2 > t1) {
           return t1;
         }
-   
+
         while (t0 < t1) {
           x2 = sampleCurveX(t2);
           if (Math.abs(x2 - x) < epsilon) {
@@ -8117,11 +8117,11 @@ ionic.views.Slider = ionic.views.View.inherit({
           }
           t2 = (t1 - t0) * 0.5 + t0;
         }
-   
+
         // Failure.
         return t2;
       };
-   
+
       /**
        * @param x {number} the value of x along the bezier curve, 0.0 <= x <= 1.0
        * @param epsilon {number} the accuracy of t for the given x
@@ -8130,9 +8130,9 @@ ionic.views.Slider = ionic.views.View.inherit({
       var solve = function(x, epsilon) {
         return sampleCurveY(solveCurveX(x, epsilon));
       };
-   
+
       // public interface --------------------------------------------
-   
+
       /**
        * Find the y of the cubic-bezier for a given x with accuracy determined by the animation duration.
        * @param x {number} the value of x along the bezier curve, 0.0 <= x <= 1.0
@@ -8143,7 +8143,7 @@ ionic.views.Slider = ionic.views.View.inherit({
         return solve(x, solveEpsilon(+duration || DEFAULT_DURATION));
       };
     };
-   
+
     // http://www.w3.org/TR/css3-transitions/#transition-timing-function
     return {
       /**
@@ -8152,35 +8152,35 @@ ionic.views.Slider = ionic.views.View.inherit({
        * @return {number} the y value along the bezier curve
        */
       linear: unitBezier(0.0, 0.0, 1.0, 1.0),
-   
+
       /**
        * @param x {number} the value of x along the bezier curve, 0.0 <= x <= 1.0
        * @param duration {number} the duration of the animation in milliseconds
        * @return {number} the y value along the bezier curve
        */
       ease: unitBezier(0.25, 0.1, 0.25, 1.0),
-   
+
       /**
        * @param x {number} the value of x along the bezier curve, 0.0 <= x <= 1.0
        * @param duration {number} the duration of the animation in milliseconds
        * @return {number} the y value along the bezier curve
        */
       easeIn: unitBezier(0.42, 0, 1.0, 1.0),
-   
+
       /**
        * @param x {number} the value of x along the bezier curve, 0.0 <= x <= 1.0
        * @param duration {number} the duration of the animation in milliseconds
        * @return {number} the y value along the bezier curve
        */
       easeOut: unitBezier(0, 0, 0.58, 1.0),
-   
+
       /**
        * @param x {number} the value of x along the bezier curve, 0.0 <= x <= 1.0
        * @param duration {number} the duration of the animation in milliseconds
        * @return {number} the y value along the bezier curve
        */
       easeInOut: unitBezier(0.42, 0, 0.58, 1.0),
-   
+
       /**
        * @param p1x {number} X component of control point 1
        * @param p1y {number} Y component of control point 1
@@ -8202,14 +8202,14 @@ ionic.views.Slider = ionic.views.View.inherit({
  */
 var Easing = (function(){
 	'use strict';
- 
+
 	/**
 	 * @const
 	 */
 	var EASE_IN_OUT_CONST = 0.5 * Math.pow(0.5, 1.925);
- 
+
 	return {
- 
+
 		/**
 		 * @param x {number} the value of x along the curve, 0.0 <= x <= 1.0
 		 * @return {number} the y value along the curve
@@ -8217,7 +8217,7 @@ var Easing = (function(){
 		linear: function(x) {
 			return x;
 		},
- 
+
 //		/**
 //		 * @param x {number} the value of x along the curve, 0.0 <= x <= 1.0
 //		 * @return {number} the y value along the curve
@@ -8226,7 +8226,7 @@ var Easing = (function(){
 //			// TODO: find fast approximations
 //			return x;
 //		},
- 
+
 		/**
 		 * @param x {number} the value of x along the curve, 0.0 <= x <= 1.0
 		 * @return {number} the y value along the curve
@@ -8235,7 +8235,7 @@ var Easing = (function(){
 			// very close approximation to cubic-bezier(0.42, 0, 1.0, 1.0)
 			return Math.pow(x, 1.685);
 		},
- 
+
 		/**
 		 * @param x {number} the value of x along the curve, 0.0 <= x <= 1.0
 		 * @return {number} the y value along the curve
@@ -8243,7 +8243,7 @@ var Easing = (function(){
 		easeInQuadratic: function(x) {
 			return (x * x);
 		},
- 
+
 		/**
 		 * @param x {number} the value of x along the curve, 0.0 <= x <= 1.0
 		 * @return {number} the y value along the curve
@@ -8251,7 +8251,7 @@ var Easing = (function(){
 		easeInCubic: function(x) {
 			return (x * x * x);
 		},
- 
+
 		/**
 		 * @param x {number} the value of x along the curve, 0.0 <= x <= 1.0
 		 * @return {number} the y value along the curve
@@ -8260,7 +8260,7 @@ var Easing = (function(){
 			// very close approximation to cubic-bezier(0, 0, 0.58, 1.0)
 			return 1 - Math.pow(1-x, 1.685);
 		},
- 
+
 		/**
 		 * @param x {number} the value of x along the curve, 0.0 <= x <= 1.0
 		 * @return {number} the y value along the curve
@@ -8269,7 +8269,7 @@ var Easing = (function(){
 			x -= 1;
 			return 1 - (x * x);
 		},
- 
+
 		/**
 		 * @param x {number} the value of x along the curve, 0.0 <= x <= 1.0
 		 * @return {number} the y value along the curve
@@ -8278,7 +8278,7 @@ var Easing = (function(){
 			x -= 1;
 			return 1 + (x * x * x);
 		},
- 
+
 		/**
 		 * @param x {number} the value of x along the curve, 0.0 <= x <= 1.0
 		 * @return {number} the y value along the curve
@@ -8287,12 +8287,12 @@ var Easing = (function(){
 			// very close approximation to cubic-bezier(0.42, 0, 0.58, 1.0)
 			if (x < 0.5) {
 				return EASE_IN_OUT_CONST * Math.pow(x, 1.925);
-	
+
 			} else {
 				return 1 - EASE_IN_OUT_CONST * Math.pow(1-x, 1.925);
 			}
 		},
- 
+
 		/**
 		 * @param x {number} the value of x along the curve, 0.0 <= x <= 1.0
 		 * @return {number} the y value along the curve
@@ -8300,13 +8300,13 @@ var Easing = (function(){
 		easeInOutQuadratic: function(x) {
 			if (x < 0.5) {
 				return (2 * x * x);
-	
+
 			} else {
 				x -= 1;
 				return 1 - (2 * x * x);
 			}
 		},
- 
+
 		/**
 		 * @param x {number} the value of x along the curve, 0.0 <= x <= 1.0
 		 * @return {number} the y value along the curve
@@ -8314,13 +8314,13 @@ var Easing = (function(){
 		easeInOutCubic: function(x) {
 			if (x < 0.5) {
 				return (4 * x * x * x);
-	
+
 			} else {
 				x -= 1;
 				return 1 + (4 * x * x * x);
 			}
 		},
- 
+
 		/**
 		 * @param x {number} the value of x along the curve, 0.0 <= x <= 1.0
 		 * @return {number} the y value along the curve
@@ -8328,13 +8328,13 @@ var Easing = (function(){
 		easeInOutQuartic: function(x) {
 			if (x < 0.5) {
 				return (8 * x * x * x * x);
-	
+
 			} else {
 				x -= 1;
 				return 1 + (8 * x * x * x * x);
 			}
 		},
- 
+
 		/**
 		 * @param x {number} the value of x along the curve, 0.0 <= x <= 1.0
 		 * @return {number} the y value along the curve
@@ -8342,7 +8342,7 @@ var Easing = (function(){
 		easeInOutQuintic: function(x) {
 			if (x < 0.5) {
 				return (16 * x * x * x * x * x);
-	
+
 			} else {
 				x -= 1;
 				return 1 + (16 * x * x * x * x * x);
@@ -8389,7 +8389,7 @@ var Easing = (function(){
       anticipationStrength: 0,
       anticipationSize: 0
     };
-    
+
     ionic.extend(this, opts);
   };
 
@@ -8429,7 +8429,7 @@ var Easing = (function(){
       //return [t, v, At, frictionT, angle];
       return v;
     }
-  }
+  };
 
   ionic.Animation.Dynamics.Gravity = function(opts) {
     this.options = {
@@ -8497,8 +8497,8 @@ var Easing = (function(){
       }
       return _results;
     },
-    curve: function(a, b, H, t){ 
-      
+    curve: function(a, b, H, t){
+
       var L, c, t2;
       L = b - a;
       t2 = (2 / L) * t - 1 - (a * 2 / L);
@@ -8529,8 +8529,8 @@ var Easing = (function(){
       //return [t, v];
       return v;
     }
-    
-  }
+
+  };
 })(window);
 
 (function(window) {
@@ -8543,43 +8543,43 @@ var Easing = (function(){
     'spring': function(duration) {
       return function(t) {
         return ionic.Animation.Dynamics.Spring(t, duration);
-      }
+      };
     },
     'gravity': function(duration) {
       return function(t) {
         return ionic.Animation.Dynamics.Gravity(t, duration);
-      }
+      };
     },
     'linear': function(duration) {
       return function(t) {
         return ionic.Animation.Bezier.linear(t, duration);
-      }
+      };
     },
     'ease': function(duration) {
       return function(t) {
         return ionic.Animation.Bezier.ease(t, duration);
-      }
+      };
     },
     'ease-in': function(duration) {
       return function(t) {
         return ionic.Animation.Bezier.easeIn(t, duration);
-      }
+      };
     },
     'ease-out': function(duration) {
       return function(t) {
         return ionic.Animation.Bezier.easeOut(t, duration);
-      }
+      };
     },
     'ease-in-out': function(duration) {
       return function(t) {
         return ionic.Animation.Bezier.easeInOut(t, duration);
-      }
+      };
     },
     'cubic-bezier': function(x1, y1, x2, y2, duration) {
       var bz = ionic.Animation.Bezier.cubicBezier(x1, y1, x2, y2);//, t, duration);
       return function(t) {
         return bz(t, duration);
-      }
+      };
     }
   };
 })(window);
@@ -8621,7 +8621,7 @@ var Easing = (function(){
       });
     },
     curve: 'linear',
-    curveFn: ionic.Animation.TimingFn['linear'],
+    curveFn: ionic.Animation.TimingFn.linear,
     duration: 500,
     delay: 0,
     repeat: -1,
@@ -8665,7 +8665,7 @@ var Easing = (function(){
     _saveState: function(now, closure) {
       this._pauseState = {
         pausedAt: now,
-      }
+      };
       this._lastStepFn = closure;
       window.cancelAnimationFrame(closure);
     },
@@ -8677,7 +8677,7 @@ var Easing = (function(){
       // TODO: Verify this isn't totally stupid
       ionic.requestAnimationFrame(function() {
         self.start();
-      })
+      });
     },
 
     start: function() {
@@ -8694,7 +8694,7 @@ var Easing = (function(){
         repeat: this.repeat,
         autoReverse: this.autoReverse,
         dynamic: this.dynamic
-      }
+      };
 
       ionic.Animation.animationStarted(this);
 
@@ -8758,7 +8758,7 @@ var Easing = (function(){
         // Start fresh either way
         start = time();
         ionic.requestAnimationFrame(step);
-      }
+      };
 
 
       // This is the internal step method which is called every few milliseconds
@@ -8846,7 +8846,11 @@ var Easing = (function(){
           } else if(repeat === 0 && autoReverse) {
             perhapsAutoreverse();
           } else {
-            completedCallback && completedCallback(desiredFrames - (dropCounter / ((now - start) / millisecondsPerSecond)), self._animationId, percent === endPercent || duration == null);
+            completedCallback && completedCallback(
+              desiredFrames - (dropCounter / ((now - start) / millisecondsPerSecond)),
+              self._animationId,
+              percent === endPercent || duration === null
+            );
           }
         } else if (render) {
           lastFrame = now;
@@ -35036,7 +35040,7 @@ angular.module('ui.router.compat')
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.5-nightly-2103
+ * Ionic, v1.0.0-beta.5-nightly-2104
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -35329,22 +35333,22 @@ jqLite.prototype.removeClass = function(cssClasses) {
  *    var anim = $ionicAnimate({
  *     // A unique, reusable name
  *     name: 'popIn',
- *     
+ *
  *     // The duration of an auto playthrough
  *     duration: 0.5,
- *     
+ *
  *     // How long to wait before running the animation
  *     delay: 0,
- *     
+ *
  *     // Whether to reverse after doing one run through
  *     autoReverse: false,
- *     
+ *
  *     // How many times to repeat? -1 or null for infinite
  *     repeat: -1,
- *     
+ *
  *     // Timing curve to use (same as CSS timing functions), or a function of time "t" to handle it yourself
  *     curve: 'ease-in-out'
- *     
+ *
  *     onStart: function() {
  *       // Callback on start
  *     },
@@ -35352,7 +35356,7 @@ jqLite.prototype.removeClass = function(cssClasses) {
  *       // Callback on end
  *     },
  *     step: function(amt) {
- *       
+ *
  *     }
  *   })
  * });
@@ -35374,8 +35378,8 @@ IonicModule
     return function(opts) {
       opts.useSlowAnimations = useSlowAnimations;
       return ionic.Animation.create(opts);
-    }
-  }]
+    };
+  }];
 });
 
 /**
