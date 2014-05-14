7bfb2c645bc285393c3e3dadfa917fb2e9dc5dc7
Ionitron <hi@ionicframework.com>
2014-05-14 18:33:17 +0000

    release: v1.0.0-beta.5-nightly-2107

--- js/ionic-angular.js
+++ js/ionic-angular.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.5-nightly-2106
+ * Ionic, v1.0.0-beta.5-nightly-2107
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
--- js/ionic.js
+++ js/ionic.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.5-nightly-2106
+ * Ionic, v1.0.0-beta.5-nightly-2107
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -19,7 +19,7 @@
 window.ionic = {
   controllers: {},
   views: {},
-  version: '1.0.0-beta.5-nightly-2106'
+  version: '1.0.0-beta.5-nightly-2107'
 };
 
 (function(ionic) {
