482f854bea568b6ab8f15563de31efb904690fbe
Ionitron <hi@ionicframework.com>
2014-06-12 22:04:01 +0000

    release: v1.0.0-beta.7-nightly-177

--- js/ionic-angular.js
+++ js/ionic-angular.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.7-nightly-176
+ * Ionic, v1.0.0-beta.7-nightly-177
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
