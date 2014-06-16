9101154144c2024f1970963f8280af9997115828
Ionitron <hi@ionicframework.com>
2014-06-16 20:56:53 +0000

    release: v1.0.0-beta.8-nightly-198

--- js/ionic.js
+++ js/ionic.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.8-nightly-197
+ * Ionic, v1.0.0-beta.8-nightly-198
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -19,7 +19,7 @@
 window.ionic = {
   controllers: {},
   views: {},
-  version: '1.0.0-beta.8-nightly-197'
+  version: '1.0.0-beta.8-nightly-198'
 };
 
 (function(ionic) {
