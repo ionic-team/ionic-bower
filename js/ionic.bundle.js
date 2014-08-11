f3be1c1f0062e1c6db5487081f8b73ddf51d0d6d
Ionitron <hi@ionicframework.com>
2014-08-11 18:59:33 +0000

    release: v1.0.0-beta.11-nightly-355

--- js/ionic.bundle.js
+++ js/ionic.bundle.js
@@ -9,7 +9,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.11-nightly-354
+ * Ionic, v1.0.0-beta.11-nightly-355
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -26,7 +26,7 @@
 window.ionic = {
   controllers: {},
   views: {},
-  version: '1.0.0-beta.11-nightly-354'
+  version: '1.0.0-beta.11-nightly-355'
 };
 
 (function(window, document, ionic) {
@@ -35072,7 +35072,7 @@ angular.module('ui.router.compat')
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.11-nightly-354
+ * Ionic, v1.0.0-beta.11-nightly-355
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
--- js/ionic.js
+++ js/ionic.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.11-nightly-354
+ * Ionic, v1.0.0-beta.11-nightly-355
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -19,7 +19,7 @@
 window.ionic = {
   controllers: {},
   views: {},
-  version: '1.0.0-beta.11-nightly-354'
+  version: '1.0.0-beta.11-nightly-355'
 };
 
 (function(window, document, ionic) {
