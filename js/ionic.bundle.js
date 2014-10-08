52c60957eabd44316ff024b3c202e33cab1dc298
Ionitron <hi@ionicframework.com>
2014-10-08 19:21:27 +0000

    release: v1.0.0-beta.13-nightly-554

--- js/ionic.bundle.js
+++ js/ionic.bundle.js
@@ -9,7 +9,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.13-nightly-551
+ * Ionic, v1.0.0-beta.13-nightly-554
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -25,7 +25,7 @@
 // build processes may have already created an ionic obj
 window.ionic = window.ionic || {};
 window.ionic.views = {};
-window.ionic.version = '1.0.0-beta.13-nightly-551';
+window.ionic.version = '1.0.0-beta.13-nightly-554';
 
 (function(window, document, ionic) {
 
@@ -886,7 +886,7 @@ window.ionic.version = '1.0.0-beta.13-nightly-551';
                       }
 
                       if(this.srcEvent.preventDefault) {
-                        this.srcEvent.preventDefault();
+                        // this.srcEvent.preventDefault();
                       }
                     },
 
@@ -34845,7 +34845,7 @@ angular.module('ui.router.compat')
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.13-nightly-551
+ * Ionic, v1.0.0-beta.13-nightly-554
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
--- js/ionic.js
+++ js/ionic.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.13-nightly-551
+ * Ionic, v1.0.0-beta.13-nightly-554
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -18,7 +18,7 @@
 // build processes may have already created an ionic obj
 window.ionic = window.ionic || {};
 window.ionic.views = {};
-window.ionic.version = '1.0.0-beta.13-nightly-551';
+window.ionic.version = '1.0.0-beta.13-nightly-554';
 
 (function(window, document, ionic) {
 
@@ -879,7 +879,7 @@ window.ionic.version = '1.0.0-beta.13-nightly-551';
                       }
 
                       if(this.srcEvent.preventDefault) {
-                        this.srcEvent.preventDefault();
+                        // this.srcEvent.preventDefault();
                       }
                     },
 
