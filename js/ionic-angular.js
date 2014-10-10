a7bd18998ea5c1c4819335a914cfe7201ff168a4
Ionitron <hi@ionicframework.com>
2014-10-10 01:27:14 +0000

    release: v1.0.0-beta.13-nightly-580

--- js/ionic-angular.js
+++ js/ionic-angular.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.13-nightly-579
+ * Ionic, v1.0.0-beta.13-nightly-580
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -7463,6 +7463,13 @@ function($animate, $timeout) {
             },
             canSwipe: function() {
               return listCtrl.canSwipeItems();
+            }
+          });
+
+          $scope.$on('$destroy', function() {
+            if(listView) {
+              listView.deregister && listView.deregister();
+              listView = null;
             }
           });
 
