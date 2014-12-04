79faa98455043ad896f4353643af6cb085f0b8d2
Ionitron <hi@ionicframework.com>
2014-12-04 20:35:01 +0000

    release: v1.0.0-beta.13-nightly-850

--- js/ionic-angular.js
+++ js/ionic-angular.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.13-nightly-849
+ * Ionic, v1.0.0-beta.13-nightly-850
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -7338,6 +7338,8 @@ function(scope, element, $log, $document, $$q, $timeout, $interval, $$ionicAttac
   // adds data to the queue for selection.
   // Index can be either a number or a getter (to be called when starting the slide)
   function select(newIndex, transitionDuration, isDrag) {
+    newIndex = parseInt(newIndex);
+    if (isNaN(newIndex) || newIndex < 0) return;
     slideQueue.unshift([
       angular.isFunction(newIndex) ? newIndex : function() { return newIndex; },
       transitionDuration || SLIDE_TRANSITION_DURATION,
--- js/ionic.bundle.js
+++ js/ionic.bundle.js
@@ -9,7 +9,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.13-nightly-849
+ * Ionic, v1.0.0-beta.13-nightly-850
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -25,7 +25,7 @@
 // build processes may have already created an ionic obj
 window.ionic = window.ionic || {};
 window.ionic.views = {};
-window.ionic.version = '1.0.0-beta.13-nightly-849';
+window.ionic.version = '1.0.0-beta.13-nightly-850';
 
 (function(window, document, ionic) {
 
@@ -39071,7 +39071,7 @@ angular.module('ui.router.compat')
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.13-nightly-849
+ * Ionic, v1.0.0-beta.13-nightly-850
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -46407,6 +46407,8 @@ function(scope, element, $log, $document, $$q, $timeout, $interval, $$ionicAttac
   // adds data to the queue for selection.
   // Index can be either a number or a getter (to be called when starting the slide)
   function select(newIndex, transitionDuration, isDrag) {
+    newIndex = parseInt(newIndex);
+    if (isNaN(newIndex) || newIndex < 0) return;
     slideQueue.unshift([
       angular.isFunction(newIndex) ? newIndex : function() { return newIndex; },
       transitionDuration || SLIDE_TRANSITION_DURATION,
