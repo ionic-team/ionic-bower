cc295e38e39cca7c7800b700a21119f29cd33e8b
Ionitron <hi@ionicframework.com>
2014-12-09 01:40:56 +0000

    release: v1.0.0-beta.13-nightly-872

--- js/ionic.bundle.js
+++ js/ionic.bundle.js
@@ -9,7 +9,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.13-nightly-871
+ * Ionic, v1.0.0-beta.13-nightly-872
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -25,7 +25,7 @@
 // build processes may have already created an ionic obj
 window.ionic = window.ionic || {};
 window.ionic.views = {};
-window.ionic.version = '1.0.0-beta.13-nightly-871';
+window.ionic.version = '1.0.0-beta.13-nightly-872';
 
 (function(window, document, ionic) {
 
@@ -39082,7 +39082,7 @@ angular.module('ui.router.compat')
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.13-nightly-871
+ * Ionic, v1.0.0-beta.13-nightly-872
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
--- js/ionic.js
+++ js/ionic.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.13-nightly-871
+ * Ionic, v1.0.0-beta.13-nightly-872
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -18,7 +18,7 @@
 // build processes may have already created an ionic obj
 window.ionic = window.ionic || {};
 window.ionic.views = {};
-window.ionic.version = '1.0.0-beta.13-nightly-871';
+window.ionic.version = '1.0.0-beta.13-nightly-872';
 
 (function(window, document, ionic) {
 
