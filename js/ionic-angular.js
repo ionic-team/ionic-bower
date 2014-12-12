0b35c2a108d6735547eb55b3ebad93f6253a403f
Ionitron <hi@ionicframework.com>
2014-12-12 17:42:32 +0000

    release: v1.0.0-beta.13-nightly-897

--- js/ionic-angular.js
+++ js/ionic-angular.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.13-nightly-895
+ * Ionic, v1.0.0-beta.13-nightly-897
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -8501,9 +8501,9 @@ function headerFooterBarDirective(isHeader) {
       controller: '$ionicHeaderBar',
       compile: function(tElement, $attr) {
         tElement.addClass(isHeader ? 'bar bar-header' : 'bar bar-footer');
-        // android style tabs? if so, remove bottom border for seamless display
+        // top style tabs? if so, remove bottom border for seamless display
         $timeout(function() {
-          if ($document[0].getElementsByClassName('tabs-top').length) tElement.addClass('has-tabs-top');
+          if (isHeader && $document[0].getElementsByClassName('tabs-top').length) tElement.addClass('has-tabs-top');
         });
 
         return { pre: prelink };
--- js/ionic.js
+++ js/ionic.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-beta.13-nightly-895
+ * Ionic, v1.0.0-beta.13-nightly-897
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -18,7 +18,7 @@
 // build processes may have already created an ionic obj
 window.ionic = window.ionic || {};
 window.ionic.views = {};
-window.ionic.version = '1.0.0-beta.13-nightly-895';
+window.ionic.version = '1.0.0-beta.13-nightly-897';
 
 (function(window, document, ionic) {
 
