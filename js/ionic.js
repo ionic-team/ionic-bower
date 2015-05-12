474c6828f920e2c2566e3bd336366fdd926b102c
Ionitron <hi@ionicframework.com>
2015-05-12 17:23:39 +0000

    release: v1.0.0

--- js/ionic.js
+++ js/ionic.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.0.0-rc.5-nightly-1296
+ * Ionic, v1.0.0
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -18,7 +18,7 @@
 // build processes may have already created an ionic obj
 window.ionic = window.ionic || {};
 window.ionic.views = {};
-window.ionic.version = '1.0.0-rc.5-nightly-1296';
+window.ionic.version = '1.0.0';
 
 (function (ionic) {
 
