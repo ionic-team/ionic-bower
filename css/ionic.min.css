dcf32c6386441deccc3b10349047f67216d3e63f
Ionitron <hi@ionicframework.com>
2015-09-11 18:09:23 +0000

    release: v1.1.0-nightly-1550

--- css/ionic.min.css
+++ css/ionic.min.css
@@ -2,7 +2,7 @@
  * Copyright 2014 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.1.0-nightly-1549
+ * Ionic, v1.1.0-nightly-1550
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
