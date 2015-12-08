15608584f2cef7ae188a8a542fb15cf912d74c02
Ionitron <hi@ionicframework.com>
2015-12-08 05:14:19 +0000

    release: v1.1.1-nightly-1790

--- js/ionic.js
+++ js/ionic.js
@@ -2,7 +2,7 @@
  * Copyright 2015 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.1.1-nightly-1786
+ * Ionic, v1.1.1-nightly-1790
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -18,7 +18,7 @@
 // build processes may have already created an ionic obj
 window.ionic = window.ionic || {};
 window.ionic.views = {};
-window.ionic.version = '1.1.1-nightly-1786';
+window.ionic.version = '1.1.1-nightly-1790';
 
 (function (ionic) {
 
@@ -2675,7 +2675,7 @@ ionic.tap = {
     if (window.navigator.pointerEnabled) {
       tapEventListener('pointerdown');
       tapEventListener('pointerup');
-      tapEventListener('pointcancel');
+      tapEventListener('pointercancel');
       tapTouchMoveListener = 'pointermove';
 
     } else if (window.navigator.msPointerEnabled) {
