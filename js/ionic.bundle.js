b967ac8c844e34658c8730d63fc3fe2c21625427
Ionitron <hi@ionicframework.com>
2015-12-10 00:38:43 +0000

    release: v1.2.0-nightly-1821

--- js/ionic.bundle.js
+++ js/ionic.bundle.js
@@ -9,7 +9,7 @@
  * Copyright 2015 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.1.1-nightly-1819
+ * Ionic, v1.2.0-nightly-1821
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -25,7 +25,7 @@
 // build processes may have already created an ionic obj
 window.ionic = window.ionic || {};
 window.ionic.views = {};
-window.ionic.version = '1.1.1-nightly-1819';
+window.ionic.version = '1.2.0-nightly-1821';
 
 (function (ionic) {
 
@@ -50190,7 +50190,7 @@ angular.module('ui.router.state')
  * Copyright 2015 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.1.1-nightly-1819
+ * Ionic, v1.2.0-nightly-1821
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
--- js/ionic.js
+++ js/ionic.js
@@ -2,7 +2,7 @@
  * Copyright 2015 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.1.1-nightly-1819
+ * Ionic, v1.2.0-nightly-1821
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -18,7 +18,7 @@
 // build processes may have already created an ionic obj
 window.ionic = window.ionic || {};
 window.ionic.views = {};
-window.ionic.version = '1.1.1-nightly-1819';
+window.ionic.version = '1.2.0-nightly-1821';
 
 (function (ionic) {
 
