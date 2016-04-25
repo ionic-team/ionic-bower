26c640ee1d73e06cca868de6a3f7a44b0c447c53
Ionitron <hi@ionicframework.com>
2016-04-25 20:03:58 +0000

    release: v1.3.0-nightly-3126

--- js/ionic-angular.js
+++ js/ionic-angular.js
@@ -2,7 +2,7 @@
  * Copyright 2015 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.3.0-nightly-3125
+ * Ionic, v1.3.0-nightly-3126
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
