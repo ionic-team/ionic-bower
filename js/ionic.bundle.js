4949e40f4fb478c47eeaa2b072f1fe1a40d9d2e5
Ionitron <hi@ionicframework.com>
2017-10-23 12:02:27 -0700

    release: v1.3.4

--- js/ionic.bundle.js
+++ js/ionic.bundle.js
@@ -9,7 +9,7 @@
  * Copyright 2015 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.3.3
+ * Ionic, v1.3.4
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
@@ -25,7 +25,7 @@
 // build processes may have already created an ionic obj
 window.ionic = window.ionic || {};
 window.ionic.views = {};
-window.ionic.version = '1.3.3';
+window.ionic.version = '1.3.4';
 
 (function (ionic) {
 
@@ -53185,7 +53185,7 @@ angular.module('ui.router.state')
  * Copyright 2015 Drifty Co.
  * http://drifty.com/
  *
- * Ionic, v1.3.3
+ * Ionic, v1.3.4
  * A powerful HTML5 mobile app framework.
  * http://ionicframework.com/
  *
